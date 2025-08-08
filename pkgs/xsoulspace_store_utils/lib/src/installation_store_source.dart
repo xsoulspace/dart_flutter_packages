a32d5a6461f0fed2ffa58017fb9604462c6e9220
arenukvern <anton@xsoulspace.dev>
2025-08-08 17:02:37 +0300

    wip: store utils

---
+++ pkgs/xsoulspace_store_utils/lib/src/installation_store_source.dart
@@ -0,0 +1,61 @@
+/// Represents the source from which the application was installed.
+enum InstallationStoreSource {
+  // android
+  androidAmazonAppStore,
+  androidGooglePlay,
+  androidGooglePlayInstaller,
+  androidHuawaiAppGallery,
+  androidRustore,
+  androidSamsungGalaxyStore,
+  androidSamsungSmartSwitchMobile,
+  androidVivoAppStore,
+  androidXiaomiGetApps,
+  androidOppoAppMarket,
+  androidApk,
+
+  // apple
+  appleIOSAppStore,
+  appleIOSTestFlight,
+  appleIOSIpa,
+  appleMacOSAppStore,
+  appleMacOSTestFlight,
+  appleMacOSDmg,
+  appleMacOSSteam,
+  appleWatchOS,
+  appleVisionOS,
+
+  // desktop/web/other
+  windowsStore,
+  windowsSteam,
+  linux,
+  linuxSnap,
+  linuxFlatpak,
+  linuxSteam,
+  fuchsia,
+  unknown,
+  webSelfhost,
+  webItchIo;
+
+  bool get isAndroid => name.startsWith('android');
+  bool get isApple => name.startsWith('apple');
+
+  bool get isAppleMacos => switch (this) {
+    InstallationStoreSource.appleMacOSAppStore ||
+    InstallationStoreSource.appleMacOSTestFlight ||
+    InstallationStoreSource.appleMacOSDmg ||
+    InstallationStoreSource.appleMacOSSteam => true,
+    _ => false,
+  };
+
+  bool get isAppleIos => switch (this) {
+    InstallationStoreSource.appleIOSAppStore ||
+    InstallationStoreSource.appleIOSTestFlight ||
+    InstallationStoreSource.appleIOSIpa => true,
+    _ => false,
+  };
+
+  bool get isWindows => name.startsWith('windows');
+  bool get isLinux => name.startsWith('linux');
+  bool get isFuchsia => name.startsWith('fuchsia');
+  bool get isWeb => name.startsWith('web');
+}
--- pkgs/xsoulspace_store_utils/lib/src/installation_target_store.dart
+++ pkgs/xsoulspace_store_utils/lib/src/installation_target_store.dart
@@ -1,29 +1,29 @@
-/// The type of purchase provider.
-enum PurchaseProviderType {
-  /// {@template purchase_provider_type_mobile_google_apple_pay}
+/// The target store or platform the app is intended for.
+enum InstallationTargetStore {
+  /// {@template installation_target_store_mobile_google_play}
   /// The purchase provider is the mobile Google Apple Pay.
   /// based on [in_app_purchase](https://pub.dev/packages/in_app_purchase)
   /// {@endtemplate}
   mobileGooglePlay('Google Play'),
 
-  /// {@template purchase_provider_type_mobile_app_store}
+  /// {@template installation_target_store_mobile_apple_app_store}
   /// The purchase provider is the mobile Apple App Store.
   /// based on [in_app_purchase](https://pub.dev/packages/in_app_purchase)
   /// {@endtemplate}
   mobileAppleAppStore('App Store'),
 
-  /// {@template purchase_provider_type_rustore}
+  /// {@template installation_target_store_rustore}
   /// The purchase provider is the rustore.
   /// {@endtemplate}
   rustore('RuStore'),
 
-  /// {@template purchase_provider_type_huawei}
+  /// {@template installation_target_store_huawei}
   /// The purchase provider is the huawei.
   /// {@endtemplate}
   huawei('Huawei AppGallery');
 
-  const PurchaseProviderType(this.storeName);
+  const InstallationTargetStore(this.name);
 
-  /// The name of the store.
-  final String storeName;
+  /// Human-friendly name of the store/target.
+  final String name;
 }
