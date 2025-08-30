5d82b219ea2f0101d3db80f2fa683055e6608939
arenukvern <anton@xsoulspace.dev>
2025-08-30 14:56:38 +0300

    fix: in_app_purchase_storekit

--- pkgs/rustore_billing_api/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pkgs/rustore_billing_api/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import in_app_purchase_storekit
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
 }
