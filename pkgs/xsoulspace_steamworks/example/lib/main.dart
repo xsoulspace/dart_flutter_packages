81b12fd3586db12937c6d5805dd6f1019a6f6a18
arenukvern <anton@xsoulspace.dev>
2026-07-24 04:57:14 +0300

    fix: warning

--- pkgs/xsoulspace_steamworks/example/lib/main.dart
+++ pkgs/xsoulspace_steamworks/example/lib/main.dart
@@ -3,7 +3,13 @@ import 'package:flutter/widgets.dart';
 import 'app.dart';
 
 void main() {
+  // ignore: do_not_use_environment
   const useFakeBackend = bool.fromEnvironment('STEAMWORKS_EXAMPLE_FAKE');
 
-  runApp(const SteamworksExampleApp());
+  runApp(
+    const SteamworksExampleApp(
+      // ignore: avoid_redundant_argument_values
+      useFakeBackend: useFakeBackend,
+    ),
+  );
 }
